7ff2cd666b6cfa673f8597b00d1d2d30992dd77f
simron <simronkang@gmail.com>
2024-07-04 16:25:58 -0400

    Add Booking API endpoint tests

---
+++ cypress/e2e/api-tests/getBookingTest.cy.js
@@ -0,0 +1,37 @@
+describe('All Booking GET Tests',()=>{
+    it('GET Request: Should return complete booking info in xml format', ()=>{
+        let bid = 465;
+        cy.request({
+            url: `https://restful-booker.herokuapp.com/booking/${bid}`,
+            method: 'GET',
+            headers:{
+                'Accept':'application/xml'
+            }
+        }).then((res)=>{
+            expect(res.status).to.equal(200);
+
+            expect(res.headers['content-type']).to.contain("text/html; charset=utf-8");
+        
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(res.body, 'application/xml');
+            const specificNode = xmlDoc.getElementsByTagName('totalprice')[0];
+            expect(specificNode.textContent).to.equal('111');
+        })
+    })
+    it('GET Reqest: Should return complete booking info in JSON format', ()=>{
+        let bid = 465;
+        cy.request({
+            url: `https://restful-booker.herokuapp.com/booking/${bid}`,
+            method: 'GET'
+        }).then((res)=>{
+            expect(res.status).to.equal(200);
+            // expect(res.body.firstname).to.eq("Josh");
+            // expect(res.body.lastname).to.eq("Allen");
+            // expect(res.body.bookingdates.checkin).to.eq("2018-01-01");
+            // expect(res.body.bookingdates.checkout).to.eq("2019-01-01");
+            expect(res.headers['content-type']).to.contain("application/json; charset=utf-8");
+            expect(res.body.totalprice).to.eq(111);
+            expect(res.body.depositpaid).to.eq(true);
+        })
+    })
+})
